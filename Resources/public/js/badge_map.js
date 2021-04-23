7dd28910d10dae6dec97132f22fb5600639fd749
Artur Jozefiak <artur.jozefiak@kuestenschmiede.gmbh>
2021-04-23 16:35:07 +0200

    Modify control of badge value for better UX

--- Resources/public/js/badge_map.js
+++ Resources/public/js/badge_map.js
@@ -1,89 +1,163 @@
 jQuery(document).ready(function () {
-  updateWishlistBadgeAtRefresh();
-
-// Update the Badge on click
-  $(".put-on-wishlist").on("click", putOnWishlistCallback);
-  $(".remove-from-wishlist").on("click", updateWishlistBadgeAtRefresh);
 
 // show badge at first pageload
-  updateWishlistBadgeAtRefresh();
+    updateWishlistBadgeAtRefresh();
+
+    const $removeFromWishlist = $('.remove-from-wishlist');
+    const $putOnWishlist = $('.put-on-wishlist');
+
+    $putOnWishlist.click(function (e) {
+        $('a span.memo-badge').text(lsAddOneToBadge());
+    });
+
+    $removeFromWishlist.click(function (e) {
+        $('a span.memo-badge').text(lsSubOneFromBadge());
+    });
+
 });
 
-function removeFromWishlistCallback (event) {
-  removeFromBadge();
+/**
+ * Stores data in LocalStorage by key and value.
+ * @param key
+ * @param value
+ */
+function lsStoreData(key, value) {
 
-};
-
-function putOnWishlistCallback (event) {
-  addToBadge();
-  $(".btn.remove-from-wishlist").on("click", removeFromWishlistCallback);
+    if (lsCheckIfKeyExists(key)) {
+        const actualValue = lsGetValueOf(key);
+        let newValue = parseInt(actualValue) + parseInt(value);
+        localStorage.setItem(key, newValue);
+    } else {
+        localStorage.setItem(key, value);
+    }
 }
 
-function deleteItemOnGlobalList (event) {
-  subWishlistItemBadge();
-};
+/**
+ * Returns the value of Merkzettel-Badge stored in localstorage.
+ * @returns {number}
+ */
+function lsGetBadgeCount() {
+    const badgeValue = localStorage.getItem('badgeCount');
+    return parseInt(badgeValue);
+}
 
+/**
+ * Gets the value in LocalStorage by key
+ * @param key
+ * @returns {string}
+ */
+function lsGetValueOf(key) {
+    return localStorage.getItem(key);
+}
 
-function updateWishlistBadgeAtRefresh() {
+/**
+ * Checks if a key exists in LocalStorage by keyname
+ * @param keyname
+ * @returns {boolean}
+ */
+function lsCheckIfKeyExists(keyname) {
+    let keyExists = false;
 
-  var getItemsRoute = '/gutesio/operator/wishlist/getItemCount';
-
-  $.get(getItemsRoute).done((data) => {
-    var countItemsServer = 0;
-    if (data.count > 0) {
-      countItemsServer = data.count;
-
-      if ($('.memo-badge').length) {
-        $('.memo-badge').remove();
-      }
-      var wishlistBadge = '<span class="badge badge-light memo-badge">' + countItemsServer + '</span>';
-      $(wishlistBadge).appendTo('a.link-memo');
+    if (localStorage.getItem(keyname) !== null) {
+        keyExists = true;
     }
-  });
+    return keyExists;
+}
+
+/**
+ * Adds one to the latest value of Merkzettel-Badge and returns the result.
+ * @returns {number}
+ */
+function lsAddOneToBadge() {
+    let sum = lsGetBadgeCount() + 1;
+    return sum;
+}
+
+/**
+ * Subtracts one from value of Merkzettel-Badge
+ * @returns {number}
+ */
+function lsSubOneFromBadge() {
+    let sub = lsGetBadgeCount() + 1;
+    return sub;
+}
+
+/**
+ * Get value of items on Merkliste, set Badge and store value in LocalStorage
+ */
+function updateWishlistBadgeAtRefresh() {
+    var getItemsRoute = '/gutesio/operator/wishlist/getItemCount';
+
+    $.get(getItemsRoute).done((data) => {
+        var countItemsServer = 0;
+        if (data.count > 0) {
+            countItemsServer = data.count;
+
+            if ($('.memo-badge').length) {
+                $('.memo-badge').remove();
+            }
+            var wishlistBadge = '<span class="badge badge-light memo-badge">' + countItemsServer + '</span>';
+            $(wishlistBadge).appendTo('a.link-memo');
+            localStorage.setItem('badgeValue', countItemsServer);
+        }
+    });
 }
 
 function addToBadge() {
-  let badgeVal = getBadgeValue();
+    let badgeVal = getBadgeValue();
 
-  badgeVal = badgeVal + 1;
+    badgeVal = badgeVal + 1;
 
-  if ($('.memo-badge').length) {
-    $('.memo-badge').remove();
-  }
+    if ($('.memo-badge').length) {
+        $('.memo-badge').remove();
+    }
 
-  var wishlistBadge = '<span class="badge badge-light memo-badge">' + badgeVal + '</span>';
-  $(wishlistBadge).appendTo('a.link-memo');
+    var wishlistBadge = '<span class="badge badge-light memo-badge">' + badgeVal + '</span>';
+    $(wishlistBadge).appendTo('a.link-memo');
 }
 
 function removeFromBadge() {
-  let badgeVal = getBadgeValue();
+    let badgeVal = getBadgeValue();
 
-  if ($('.memo-badge').length) {
-    $('.memo-badge').remove();
-  }
+    if ($('.memo-badge').length) {
+        $('.memo-badge').remove();
+    }
 
-  if (badgeVal > 0) {
-    badgeVal = badgeVal - 1;
-    var wishlistBadge = '<span class="badge badge-light memo-badge">' + badgeVal + '</span>';
-    $(wishlistBadge).appendTo('a.link-memo');
+    if (badgeVal > 0) {
+        badgeVal = badgeVal - 1;
+        var wishlistBadge = '<span class="badge badge-light memo-badge">' + badgeVal + '</span>';
+        $(wishlistBadge).appendTo('a.link-memo');
 
-  }
+    }
 }
 
+function removeFromWishlistCallback(event) {
+    removeFromBadge();
+};
+
+function putOnWishlistCallback(event) {
+    addToBadge();
+    $(".btn.remove-from-wishlist").on("click", removeFromWishlistCallback);
+}
+
+function deleteItemOnGlobalList(event) {
+    subWishlistItemBadge();
+};
+
 function getBadgeValue() {
-  let valBadge = 0;
-  if ($('.memo-badge').length) {
-    valBadge = parseInt($('.memo-badge').text());
-  }
-  return valBadge;
+    let valBadge = 0;
+    if ($('.memo-badge').length) {
+        valBadge = parseInt($('.memo-badge').text());
+    }
+    return valBadge;
 }
 
 window.c4gMapsHooks = window.c4gMapsHooks || {};
 window.c4gMapsHooks.proxy_fillPopup = window.c4gMapsHooks.proxy_fillPopup || [];
 
-window.c4gMapsHooks.proxy_fillPopup.push(function(params) {
-  window.setTimeout(function () {
-    jQuery(".put-on-wishlist").on("click", putOnWishlistCallback);
-    jQuery(".remove-from-wishlist").on("click", removeFromWishlistCallback);
-  }, 100);
+window.c4gMapsHooks.proxy_fillPopup.push(function (params) {
+    window.setTimeout(function () {
+        jQuery(".put-on-wishlist").on("click", putOnWishlistCallback);
+        jQuery(".remove-from-wishlist").on("click", removeFromWishlistCallback);
+    }, 100);
 });

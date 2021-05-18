eda276d504505b2dea430d2c28c19a5dd20a102c
Artur Jozefiak <artur.jozefiak@kuestenschmiede.gmbh>
2021-05-18 14:41:56 +0200

    load only necessary bootstrap js files for detail page

--- Controller/ShowcaseDetailModuleController.php
+++ Controller/ShowcaseDetailModuleController.php
@@ -110,7 +110,9 @@ class ShowcaseDetailModuleController extends \Contao\CoreBundle\Controller\Front
         $redirectUrl = $this->generator->generate("tl_page." . $redirectPage);
         ResourceLoader::loadJavaScriptResource("/bundles/con4gisframework/build/c4g-framework.js?v=" . time(), ResourceLoader::BODY, "c4g-framework");
         ResourceLoader::loadCssResource("/bundles/gutesiooperator/dist/css/c4g_detail.min.css");
-        ResourceLoader::loadJavaScriptResource("/bundles/gutesiooperator/vendor/bootstrap/button.js|async|static?v=" . time(), ResourceLoader::JAVASCRIPT, "boostrap-buttons");
+        ResourceLoader::loadJavaScriptResource("/bundles/gutesiooperator/vendor/jquery/jquery-3.5.1.slim.min.js|async|static?v=" . time(), ResourceLoader::JAVASCRIPT, "boostrap-jquery");
+        ResourceLoader::loadJavaScriptResource("/bundles/gutesiooperator/vendor/bootstrap/util.js|async|static?v=" . time(), ResourceLoader::JAVASCRIPT, "boostrap-util");
+        ResourceLoader::loadJavaScriptResource("/bundles/gutesiooperator/vendor/bootstrap/modal.js|async|static?v=" . time(), ResourceLoader::JAVASCRIPT, "boostrap-modal");
         ResourceLoader::loadJavaScriptResource("/bundles/gutesiooperator/dist/js/c4g_all.js|async|static?v=" . time(), ResourceLoader::JAVASCRIPT, "c4g-all");
         ResourceLoader::loadCssResource("/bundles/gutesiooperator/vendor/fancybox/jquery.fancybox.min.css");
         ResourceLoader::loadJavaScriptResource("/bundles/gutesiooperator/vendor/fancybox/jquery.fancybox.min.js|async|static");

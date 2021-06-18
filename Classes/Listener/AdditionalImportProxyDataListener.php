94c27151ac1bb377aeadff7163ac604e6fbefa83
Unknown <malte.de-vries@kuestenschmiede.gmbh>
2021-06-18 11:49:43 +0200

    send statistic visits data

--- Classes/Listener/AdditionalImportProxyDataListener.php
+++ Classes/Listener/AdditionalImportProxyDataListener.php
@@ -10,6 +10,7 @@
 namespace gutesio\OperatorBundle\Classes\Listener;
 
 use con4gis\CoreBundle\Classes\Events\AdditionalImportProxyDataEvent;
+use Contao\Database;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Contao\System;
 
@@ -17,9 +18,16 @@ class AdditionalImportProxyDataListener
 {
     public function importProxyData(AdditionalImportProxyDataEvent $event, $eventName, EventDispatcherInterface $eventDispatcher)
     {
+        $db = Database::getInstance();
+
         $installedPackages = System::getContainer()->getParameter('kernel.packages');
         $operatorVersion = $installedPackages['gutesio/operator'];
         $dataModelVersion = $installedPackages['gutesio/data-model'];
+
+        $offerStatistic = $db->prepare("SELECT * FROM tl_gutesio_offer_statistic")
+            ->execute()->fetchAllAssoc();
+        $showcaseStatistic = $db->prepare("SELECT * FROM tl_gutesio_showcase_statistic")
+            ->execute()->fetchAllAssoc();
 
         $proxyData = [
             [
@@ -31,6 +39,24 @@ class AdditionalImportProxyDataListener
                 'proxyData' => $dataModelVersion,
             ],
         ];
+
+        if ($offerStatistic) {
+            foreach ($offerStatistic as $statisticEntry) {
+                $proxyData[] = [
+                    "proxyKey" => "offerStatistic_".$statisticEntry['id'],
+                    "proxyData" =>  $statisticEntry['uuid'].",".$statisticEntry['date'].",".$statisticEntry['offerId'].",".$statisticEntry['visits'].",".$statisticEntry['ownerId']
+                ];
+            }
+        }
+        if ($showcaseStatistic) {
+            foreach ($showcaseStatistic as $statisticEntry) {
+                $proxyData[] = [
+                    "proxyKey" => "showcaseStatistic_".$statisticEntry['id'],
+                    "proxyData" =>  $statisticEntry['uuid'].",".$statisticEntry['date'].",".$statisticEntry['offerId'].",".$statisticEntry['visits'].",".$statisticEntry['ownerId']
+                ];
+            }
+        }
+
         $event->setProxyData($proxyData);
     }
 }

801692c19c018d24308d3990f49073642de194e8
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2022-02-18 12:15:50 +0100

    Fix php warnings

--- Resources/contao/dca/tl_c4g_import_data.php
+++ Resources/contao/dca/tl_c4g_import_data.php
@@ -15,9 +15,9 @@ if ($packages['gutesio/operator']) {
         'label'                   => &$GLOBALS['TL_LANG']['tl_c4g_import_data']['type'],
         'inputType'               => 'select',
         'options'                 => array(
-            'demo'                  => $GLOBALS['TL_LANG']['tl_c4g_import_data']['type_demo'],
-            'basedata'              => $GLOBALS['TL_LANG']['tl_c4g_import_data']['type_basedata'],
-            'gutesio'               => $GLOBALS['TL_LANG']['tl_c4g_import_data']['type_gutesio']
+            'demo'                  => &$GLOBALS['TL_LANG']['tl_c4g_import_data']['type_demo'],
+            'basedata'              => &$GLOBALS['TL_LANG']['tl_c4g_import_data']['type_basedata'],
+            'gutesio'               => &$GLOBALS['TL_LANG']['tl_c4g_import_data']['type_gutesio']
         ),
         'default'                 => '',
         'sorting'                 => true,
@@ -30,9 +30,9 @@ if ($packages['gutesio/operator']) {
         'label'                   => &$GLOBALS['TL_LANG']['tl_c4g_import_data']['source'],
         'inputType'               => 'select',
         'options'                 => array(
-            'locale'                => $GLOBALS['TL_LANG']['tl_c4g_import_data']['source_locale'],
-            'io'                    => $GLOBALS['TL_LANG']['tl_c4g_import_data']['source_io'],
-            'gutesio'               => $GLOBALS['TL_LANG']['tl_c4g_import_data']['source_gutesio']
+            'locale'                => &$GLOBALS['TL_LANG']['tl_c4g_import_data']['source_locale'],
+            'io'                    => &$GLOBALS['TL_LANG']['tl_c4g_import_data']['source_io'],
+            'gutesio'               => &$GLOBALS['TL_LANG']['tl_c4g_import_data']['source_gutesio']
         ),
         'default'                 => '',
         'sorting'                 => true,

3bb282b8264baef657012c0ff2bdf29cde01cd46
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2022-06-07 18:05:07 +0200

    Show cart module even if user is not logged in

--- Controller/CartModuleController.php
+++ Controller/CartModuleController.php
@@ -9,68 +9,21 @@
 
 namespace gutesio\OperatorBundle\Controller;
 
-
-use con4gis\CoreBundle\Classes\C4GUtils;
 use con4gis\CoreBundle\Classes\ResourceLoader;
-use con4gis\CoreBundle\Resources\contao\models\C4gLogModel;
 use con4gis\CoreBundle\Resources\contao\models\C4gSettingsModel;
-use con4gis\FrameworkBundle\Classes\FormButtons\FilterButton;
-use con4gis\FrameworkBundle\Classes\FormFields\DateRangeField;
-use con4gis\FrameworkBundle\Classes\FormFields\HiddenFormField;
-use con4gis\FrameworkBundle\Classes\FormFields\MultiCheckboxWithImageLabelFormField;
-use con4gis\FrameworkBundle\Classes\FormFields\NumberRangeFormField;
-use con4gis\FrameworkBundle\Classes\FormFields\RadioGroupFormField;
-use con4gis\FrameworkBundle\Classes\FormFields\RequestTokenFormField;
-use con4gis\FrameworkBundle\Classes\FormFields\SelectFormField;
-use con4gis\FrameworkBundle\Classes\FormFields\TextAreaFormField;
-use con4gis\FrameworkBundle\Classes\FormFields\TextFormField;
-use con4gis\FrameworkBundle\Classes\Forms\Form;
-use con4gis\FrameworkBundle\Classes\Forms\ToggleableForm;
-use con4gis\FrameworkBundle\Classes\FrontendConfiguration;
-use con4gis\FrameworkBundle\Classes\SearchConfiguration;
-use con4gis\FrameworkBundle\Classes\TileFields\HeadlineTileField;
-use con4gis\FrameworkBundle\Classes\TileFields\ImageTileField;
-use con4gis\FrameworkBundle\Classes\TileFields\LinkButtonTileField;
-use con4gis\FrameworkBundle\Classes\TileFields\LinkTileField;
-use con4gis\FrameworkBundle\Classes\TileFields\ModalButtonTileField;
-use con4gis\FrameworkBundle\Classes\TileFields\TagTileField;
-use con4gis\FrameworkBundle\Classes\TileFields\TextTileField;
-use con4gis\FrameworkBundle\Classes\TileFields\TileField;
-use con4gis\FrameworkBundle\Classes\TileFields\WrapperTileField;
-use con4gis\FrameworkBundle\Classes\TileLists\TileList;
-use con4gis\FrameworkBundle\Classes\Utility\RegularExpression;
-use con4gis\FrameworkBundle\Traits\AutoItemTrait;
-use Contao\Config;
-use Contao\Controller;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
-use Contao\CoreBundle\Exception\RedirectResponseException;
 use Contao\CoreBundle\Framework\ContaoFramework;
-use Contao\CoreBundle\Security\Authentication\Token\TokenChecker;
-use Contao\Database;
-use Contao\FilesModel;
 use Contao\ModuleModel;
-use Contao\PageModel;
-use Contao\StringUtil;
-use Contao\System;
 use Contao\Template;
-use gutesio\DataModelBundle\Resources\contao\models\GutesioDataChildVoucherModel;
-use gutesio\OperatorBundle\Classes\Models\GutesioOperatorSettingsModel;
-use gutesio\OperatorBundle\Classes\Services\OfferLoaderService;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CartModuleController extends AbstractFrontendModuleController
 {
-    private TokenChecker $tokenChecker;
-    
     public function __construct(
-        ContaoFramework $framework,
-        TokenChecker $tokenChecker
+        ContaoFramework $framework
     ) {
         $framework->initialize();
-        $this->tokenChecker = $tokenChecker;
     }
 
     protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
@@ -81,12 +34,10 @@ class CartModuleController extends AbstractFrontendModuleController
         );
 
         $con4gisSettings = C4gSettingsModel::findSettings();
-        
-        if ($this->tokenChecker->hasFrontendUser()) {
-            $template->getCartUrl = '/gutesio/operator/cart/items';
-            $template->cart_payment_url = rtrim($con4gisSettings->con4gisIoUrl, '/') . '/cart.php';
-            $template->cart_no_items_text = nl2br($model->cart_no_items_text);
-        }
+
+        $template->getCartUrl = 'http://gutes.localhost/gutesio/main/cart/items';
+        $template->cart_payment_url = rtrim($con4gisSettings->con4gisIoUrl, '/') . '/cart.php';
+        $template->cart_no_items_text = nl2br($model->cart_no_items_text);
 
         return $template->getResponse();
     }

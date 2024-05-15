0b44b412232fd5cf0911554167a53120ca521c6e
Marc <marc.zakrzewski@kuestenschmiede.gmbh>
2024-05-15 12:48:56 +0200

    add 3 fields for the pn in archive

--- src/Resources/contao/languages/de/tl_news_archive.php
+++ src/Resources/contao/languages/de/tl_news_archive.php
@@ -2,7 +2,6 @@
 
 $str = 'tl_news_archive';
 
-$GLOBALS['TL_LANG'][$str]['generateGutesBlog'] = ['Push Benachrichtigungen für kommende gutes Veranstaltungen Activieren', 'Wähle aus, welche kommenden "gutes" Event die Abonnenten mit einer Push-Nachricht benachrichtigen soll. z.B. "heute" oder "nächster Tag".'];
 $GLOBALS['TL_LANG'][$str]['subscriptionTypes'] = ['Abonnement-Typen', 'Wähle die Abonnementtypen, bei denen das Kontrollkästchen "Push Benachrichtigung für heutige Events" aktiviert ist. Die Abonnenten werden benachrichtigt'];
 
 $GLOBALS['TL_LANG'][$str]['references']['today'] = ['Heute'];
@@ -13,3 +12,7 @@ $GLOBALS['TL_LANG'][$str]['references']['nextWeek']= ['Nächste Woche'];
 $GLOBALS['TL_LANG'][$str]['references']['nextMonth']= ['Nächsten Monat'];
 
 $GLOBALS['TL_LANG'][$str]['operator_legend'] = 'Guteseinstellungen (gutesio/operator)';
+$GLOBALS['TL_LANG'][$str]['generateGutesBlog'] = ['Push Benachrichtigungen für kommende gutes Veranstaltungen Activieren', 'Wähle aus, welche kommenden "gutes" Event die Abonnenten mit einer Push-Nachricht benachrichtigen soll. z.B. "heute" oder "nächster Tag".'];
+$GLOBALS['TL_LANG'][$str]['gutesBlogTitle'] = ['Push benachrichtigung title', ' Fügen Sie den Titel für die Push-Benachrichtigung hinzu, z. B. „Achtung ~ Neue Veranstaltungen“. Diese Nachricht wird nur gesendet, wenn es gutes Veranstaltungen gibt'];
+$GLOBALS['TL_LANG'][$str]['gutesBlogTeaser'] = ['Gutes kurzer Teaser', ' Fügen Sie ein paar Sätze für die Push-Benachrichtigung hinzu, z. B. „Schau dir die lokalen Veranstaltungen an, die morgen stattfinden“. Dies wird nur gesendet, wenn es gute Veranstaltungen gibt'];
+$GLOBALS['TL_LANG'][$str]['gutesBlogImage'] = ['Benachrichtigungs-Icon', 'Hier können Sie ein Icon hinterlegen, was im Benachrichtigungsbanner angezeigt wird.'];

c6b9b8ac851ea5bd231abf93c0f366b88e996d9d
croastforge <claudio.ross@kuestenschmiede.gmbh>
2025-04-07 16:07:26 +0200

    Register cronjob in services.yml, remove old config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -15,7 +15,3 @@ if (!empty($GLOBALS['BE_MOD']['gutesio'])) {
 }
 
 $GLOBALS['TL_MODELS']['tl_gutesio_operator_settings'] = \gutesio\OperatorBundle\Classes\Models\GutesioOperatorSettingsModel::class;
-
-$GLOBALS['TL_CRON']['minutely'][] = [\gutesio\OperatorBundle\Classes\Cron\SyncDataCron::class, 'onMinutely'];
-$GLOBALS['TL_CRON']['hourly'][] = [\gutesio\OperatorBundle\Classes\Cron\SendStatisticDataCron::class, 'onHourly'];
-//$GLOBALS['TL_CRON']['hourly'][] = [\gutesio\OperatorBundle\Classes\Cron\GutesBlogGenerator::class, 'onHourly'];

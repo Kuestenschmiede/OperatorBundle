5996e8e2e7e6c91683928c1453069ad11952c81d
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2025-04-10 10:34:48 +0200

    Rename cache caption

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,4 +1,4 @@
 <?php
 $GLOBALS['TL_LANG']['PTY']['showcase_detail_module'] = ['Showcase Details', 'Example page type.'];
 
-$GLOBALS['TL_LANG']['tl_maintenance_jobs']['gutesio_offer_data'] = ['Nordsee.digital-Inhaltscache leeren', 'Löscht die zwischengespeicherten Daten von Inhalten.'];
+$GLOBALS['TL_LANG']['tl_maintenance_jobs']['gutesio_offer_data'] = ['gutes.digital-Inhaltscache leeren', 'Löscht die zwischengespeicherten Daten von Inhalten.'];

9fede897b55a48ff623a5b9da130bffdadaa9c3d
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2025-05-07 10:39:16 +0200

    Version preps

--- src/Classes/Listener/LoadLayersListener.php
+++ src/Classes/Listener/LoadLayersListener.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file belongs to gutes.digital and is published exclusively for use
+ * in gutes.digital operator or provider pages.
+
+ * @package    gutesio
+ * @copyright  Küstenschmiede GmbH Software & Design (Matthias Eilers)
+ * @link       https://gutes.digital
+ */
 namespace gutesio\OperatorBundle\Classes\Listener;
 
 use con4gis\CoreBundle\Classes\C4GUtils;

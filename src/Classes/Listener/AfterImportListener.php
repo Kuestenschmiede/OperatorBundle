505d6c5368be47a7cb5e857bd0ba68866a6a83ec
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2026-02-10 12:36:17 +0100

    Revert "Call locstyle repair service"
    This reverts commit 61cc7d8e7bb77a7051fc437eac57fb5f3c1634da.

--- src/Classes/Listener/AfterImportListener.php
+++ src/Classes/Listener/AfterImportListener.php
@@ -16,7 +16,6 @@ use con4gis\MapsBundle\Classes\Caches\C4GMapsAutomator;
 use Contao\System;
 use gutesio\DataModelBundle\Classes\Cache\ImageCache;
 use gutesio\DataModelBundle\Classes\ChildFullTextContentUpdater;
-use gutesio\DataModelBundle\Classes\LocstyleRepairService;
 use Monolog\Handler\GroupHandler;
 use Symfony\Bridge\Monolog\Logger;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -59,9 +58,6 @@ class AfterImportListener
             $importType = $event->getImportType();
             $createRobots = false;
             if ($importType == 'gutesio') {
-                $repair = new LocstyleRepairService();
-                $repair->repair();
-
                 $contentUpdate = new ChildFullTextContentUpdater();
                 $contentUpdate->update();
 
